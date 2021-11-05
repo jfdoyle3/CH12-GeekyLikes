9a75775cb9a1800a3dd158c9851eed6ca209191d
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-11-05 01:02:54 -0400

    added class project

---
+++ src/main/java/com/careerdevs/geekylikes/GeekylikesApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.geekylikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekylikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekylikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/geekylikes/GeekylikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.geekylikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekylikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
